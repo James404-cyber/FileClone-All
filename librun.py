3dac66c0517af74fa19918d55283c62a40a4fc28
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-13 07:42:52 +0300

    Update librun.py

--- librun.py
+++ librun.py
@@ -2,7 +2,7 @@ import platform
 
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Fixx32")._site_view_()
+	__import__("fixx32")._site_view_()
 elif 'aarch' in arc:
 	__import__("fixx")._site_view_()
 else:
