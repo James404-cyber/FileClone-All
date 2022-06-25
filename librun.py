9b4ca045e65a8550bcf17c36bcf39840a1a6f93d
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-25 19:41:10 +0300

    Update librun.py

--- librun.py
+++ librun.py
@@ -4,6 +4,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("fixx32")._site_view_()
 elif 'aarch' in arc:
-	__import__("fixx")._site_view_()
+	__import__("Fil")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
