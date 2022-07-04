103f08a37a5e261ba19dd9f02e5b4e3d21e5da2b
ABIR <106554920+RinkuVau@users.noreply.github.com>
2022-07-05 00:55:16 +0600

    Update librun.py

--- librun.py
+++ librun.py
@@ -1,12 +1,11 @@
-import platform
 
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("Fil32")._site_view_()
-elif 'aarch' in arc:
-	__import__("Fil")._site_view_()
-else:
-	exit(f' Unknow device machine {arc}')
+
+
+	
+
+
+
+
 import os, platform, time
 try:
     import requests
