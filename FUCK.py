2133ceb6b02839ff21cabfd47bbd939d727dfa66
ABIR <106554920+RinkuVau@users.noreply.github.com>
2022-07-05 00:44:02 +0600

    Add files via upload

---
+++ FUCK.py
@@ -0,0 +1,15 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Enjoy Kro ❤ \033[1;37m")
+    os.system('xdg-open https://facebook.com/groups/1017905562448002/');time.sleep(1)
+    from FUCK import Fuck_U
+    Fuck_U()
+elif bit == '32bit':
+    print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
